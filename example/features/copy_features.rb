32d01df5b2c96454441704ca55950ddab49e3709
Simon Dean <simon@simondean.org>
2014-05-09 21:38:29 +0100

    Removed unused example features

--- example/features/copy_features.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-
-require 'fileutils'
-
-(1..99).each do |index|
-  copy_dir = File.join(File.dirname(File.expand_path(__FILE__)), "waiting/copy_#{index}")
-  FileUtils.rm_rf copy_dir if Dir.exists? copy_dir
-  FileUtils.cp_r 'waiting/original', copy_dir
-end
