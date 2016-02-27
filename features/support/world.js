e7f1933ace0126d456e6850a44519761412a432a
Dyana Rose <dyana.rose@salecycle.com>
2016-02-27 10:44:16 +0000

    updating to integrate with cucumber.js 0.9.x

--- features/support/world.js
+++ features/support/world.js
@@ -1,11 +1,7 @@
 module.exports = function() {
-  this.World = function(callback) {
-    var world = {};
-
-    world.isDryRun = function() {
+  this.World = function() {
+    this.isDryRun = function() {
       return process.argv.indexOf('--dry-run') !== -1 || process.env.PARALLEL_CUCUMBER_DRY_RUN === 'true';
     };
-
-    callback(world);
   };
-} ;
+};
